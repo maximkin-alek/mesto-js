90b79afb64bc8837d45df2bed9e41b6ac827f9cc
Alexander Maximkin <maximkin.alek@yandex.ru>
2020-07-12 14:25:26 +0300

    fix image bug

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ module.exports = {
                 {
                     loader: 'file-loader',
                     options: {
+
                         name: './images/[name].[ext]',
                         esModule: false
                     }
